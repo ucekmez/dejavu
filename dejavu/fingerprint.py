85407577bca10a892f0c2fbbdda7768037fe7479
rootkc <info@kennethchristensen.dk>
2019-08-05 14:57:03 +0200

    add environment value support to fingerprint values

--- dejavu/fingerprint.py
+++ dejavu/fingerprint.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+import os
 import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
@@ -18,41 +19,41 @@ IDX_TIME_J = 1
 ######################################################################
 # Sampling rate, related to the Nyquist conditions, which affects
 # the range frequencies we can detect.
-DEFAULT_FS = 44100
+DEFAULT_FS = os.getenv('DEFAULT_FS', 44100)
 
 ######################################################################
 # Size of the FFT window, affects frequency granularity
-DEFAULT_WINDOW_SIZE = 4096
+DEFAULT_WINDOW_SIZE = os.getenv('DEFAULT_WINDOW_SIZE', 4096)
 
 ######################################################################
 # Ratio by which each sequential window overlaps the last and the
 # next window. Higher overlap will allow a higher granularity of offset
 # matching, but potentially more fingerprints.
-DEFAULT_OVERLAP_RATIO = 0.5
+DEFAULT_OVERLAP_RATIO = os.getenv('DEFAULT_OVERLAP_RATIO', 0.5)
 
 ######################################################################
 # Degree to which a fingerprint can be paired with its neighbors --
 # higher will cause more fingerprints, but potentially better accuracy.
-DEFAULT_FAN_VALUE = 15
+DEFAULT_FAN_VALUE = os.getenv('DEFAULT_FAN_VALUE', 15)
 
 ######################################################################
 # Minimum amplitude in spectrogram in order to be considered a peak.
 # This can be raised to reduce number of fingerprints, but can negatively
 # affect accuracy.
-DEFAULT_AMP_MIN = 10
+DEFAULT_AMP_MIN = os.getenv('DEFAULT_AMP_MIN', 10)
 
 ######################################################################
 # Number of cells around an amplitude peak in the spectrogram in order
 # for Dejavu to consider it a spectral peak. Higher values mean less
 # fingerprints and faster matching, but can potentially affect accuracy.
-PEAK_NEIGHBORHOOD_SIZE = 20
+PEAK_NEIGHBORHOOD_SIZE = os.getenv('PEAK_NEIGHBORHOOD_SIZE', 20)
 
 ######################################################################
 # Thresholds on how close or far fingerprints can be in time in order
 # to be paired as a fingerprint. If your max is too low, higher values of
 # DEFAULT_FAN_VALUE may not perform as expected.
-MIN_HASH_TIME_DELTA = 0
-MAX_HASH_TIME_DELTA = 200
+MIN_HASH_TIME_DELTA = os.getenv('MIN_HASH_TIME_DELTA', 0)
+MAX_HASH_TIME_DELTA = os.getenv('MAX_HASH_TIME_DELTA', 200)
 
 ######################################################################
 # If True, will sort peaks temporally for fingerprinting;
@@ -64,7 +65,7 @@ PEAK_SORT = True
 # Number of bits to throw away from the front of the SHA1 hash in the
 # fingerprint calculation. The more you throw away, the less storage, but
 # potentially higher collisions and misclassifications when identifying songs.
-FINGERPRINT_REDUCTION = 20
+FINGERPRINT_REDUCTION = os.getenv('FINGERPRINT_REDUCTION', 20)
 
 
 def fingerprint(
